1cf6d613c072c5656b8a4ca93dff2b83d8b2c7fc
KennyJ34 <joneskenneth34@gmail.com>
2016-12-16 04:40:16 +0000

    Adding home to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_param, only: [:edit,:show,:update,:destroy]
   
   def index
     @articles = Article.all
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
@@ -23,7 +24,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
@@ -34,11 +34,10 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def destroy
-    @article = Article.find(params[:id])
     if  @article.destroy
       flash[:notice] = "Article was successfully Destroyed"
       redirect_to articles_path
@@ -48,6 +47,9 @@ class ArticlesController < ApplicationController
   end
   
   private
+    def set_param
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title,:description)
     end
