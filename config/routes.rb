a1b4430e7b7af4b2afc1c86999b6591a2fcdfc35
KennyJ34 <joneskenneth34@gmail.com>
2016-12-16 01:11:31 +0000

    add article path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-root 'pages#home'
-get 'about', to: 'pages#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
